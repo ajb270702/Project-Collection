828936cb30ddce85607922f9c29d2cde239f1e56
ajb270702 <akhiljohnboby@gmail.com>
2025-02-15 20:23:14 +1100

    Create ram.h

---
+++ Sum 100/ram.h
@@ -0,0 +1,27 @@
+// // // // // // // DO NOT MODIFY THIS FILE! // // // // // // // // //
+
+#include <stdio.h>
+#include <stdint.h>
+
+#ifndef CS1521_ASS1__RAM_H
+#define CS1521_ASS1__RAM_H
+
+//
+// You can call these functions from `execute_instruction.c':
+//
+uint8_t get_byte(uint32_t address);
+void set_byte(uint32_t address, uint8_t value);
+
+
+//
+// These functions are used in `emu.c' --- do not call these functions.
+//
+void read_program(FILE *f);
+int  execute_next_instruction(uint32_t *program_counter);
+void print_instruction_at_address(uint32_t address);
+void print_program(void);
+void print_text_segment(void);
+void print_data_segment(void);
+void print_stack_segment(void);
+
+#endif // !defined(CS1521_ASS1__RAM_H)
