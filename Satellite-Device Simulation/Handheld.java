263b3733cb8bb30bf1871cb5387ae36c604fef47
ajb270702 <akhiljohnboby@gmail.com>
2025-02-15 21:16:50 +1100

    Create Handheld.java

---
+++ Satellite-Device Simulation/Handheld.java
@@ -0,0 +1,25 @@
+package unsw.blackout;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+public class Handheld extends Device {
+
+    public Handheld(String id, String type, double position) {
+        super(id, type, position);
+
+    }
+
+    @Override
+    public void processOptions(ArrayList<Satellite> possibleConnections, LocalTime currentTime) {
+        if (inActivationPeriod(currentTime) && !getIsConnected()) {
+            for (Satellite s : possibleConnections) {
+                if (s.canConnect(this)) {
+                    s.connect(currentTime, this, 1);
+                    setConnected(true);
+                    break;
+                }
+            }
+        }
+    }
+}
