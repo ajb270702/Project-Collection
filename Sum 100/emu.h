247a9c0cbba1421a09efaa43dbcff1ab30f11573
ajb270702 <akhiljohnboby@gmail.com>
2025-02-15 20:13:56 +1100

    Create emu.h

---
+++ Sum 100/emu.h
@@ -0,0 +1,14 @@
+// // // // // // // DO NOT MODIFY THIS FILE! // // // // // // // // //
+
+#include <stdint.h>
+
+#ifndef CS1521_ASS1__EMU_H
+#define CS1521_ASS1__EMU_H
+
+//
+// You need to write these functions.  See the specification for details.
+//
+int execute_instruction(uint32_t instruction, uint32_t *program_counter);
+void print_instruction(uint32_t instruction);
+
+#endif // !defined(CS1521_ASS1__EMU_H)
